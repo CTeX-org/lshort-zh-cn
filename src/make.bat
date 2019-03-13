f6ea09b8b06dcf55bca2509f1b70bb4aad2ab673
Liam Huang <liamhuang0205@gmail.com>
2019-03-13 14:58:32 +0800

    [make] update make.bat.

--- src/make.bat
+++ src/make.bat
@@ -1,17 +1,18 @@
 @echo off
 set LSHORT=lshort-zh-cn
 set PDF=%LSHORT%.pdf
-set TEMP=%LSHORT%.aux %LSHORT%.log %LSHORT%.idx %LSHORT%.ind %LSHORT%.ilg %LSHORT%.out %LSHORT%.toc %LSHORT%.los %LSHORT%-example.aux %LSHORT%.synctex.gz
+set TEMP=%LSHORT%.xdv %LSHORT%.aux %LSHORT%.log %LSHORT%.idx %LSHORT%.ind %LSHORT%.ilg %LSHORT%.out %LSHORT%.toc %LSHORT%.los %LSHORT%-example.aux %LSHORT%.synctex.gz
 
 if "%1"=="clean" goto clean
 
 set TEX=xelatex
+set NOPDFMODE=-interaction=nonstopmode -synctex=1 --no-pdf
 set MODE=-interaction=nonstopmode -synctex=1
 set MAKEINDEX=makeindex
 
-%TEX% %MODE% %LSHORT%
+%TEX% %NOPDFMODE% %LSHORT%
 %MAKEINDEX% -s %LSHORT%.ist %LSHORT%
-%TEX% %MODE% %LSHORT%
+%TEX% %NOPDFMODE% %LSHORT%
 %TEX% %MODE% %LSHORT%
 
 if exist %PDF% (
