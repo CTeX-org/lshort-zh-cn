aa1bfc3785cd3b60867374762592fb4ac8b085aa
louisstuart96 <louisstuart96@gmail.com>
2019-10-04 01:14:31 +0800

    Optimize `make.bat`

--- src/make.bat
+++ src/make.bat
@@ -1,9 +1,11 @@
 @echo off
+cd %~dp0
 set LSHORT=lshort-zh-cn
 set PDF=%LSHORT%.pdf
 set TEMP=%LSHORT%.xdv %LSHORT%.aux %LSHORT%.log %LSHORT%.idx %LSHORT%.ind %LSHORT%.ilg %LSHORT%.out %LSHORT%.toc %LSHORT%.los %LSHORT%-example.aux %LSHORT%.synctex.gz
 
 if "%1"=="clean" goto clean
+if "%1"=="distclean" goto distclean
 
 set TEX=xelatex
 set NOPDFMODE=-interaction=nonstopmode -synctex=1 --no-pdf
@@ -18,15 +20,15 @@ set MAKEINDEX=makeindex
 if exist %PDF% (
 copy %PDF% ..
 )
-exit
+exit /B
 
 :clean
 del %TEMP%
 del %PDF%
-exit
+exit /B
 
 :distclean
 del %TEMP%
 del %PDF%
 del ..\%PDF%
-exit
+exit /B
